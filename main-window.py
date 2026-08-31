707136c208a644248bd351936e90027c0c48f5d8
Jane Doe <paul.d.kolz@gmail.com>
2026-08-31 10:03:48 -0500

    Update main-window.py

--- main-window.py
+++ main-window.py
@@ -9,4 +9,14 @@ pygame.display.set_caption("PLATFORMER")
 
 
 
-class playerSpriteClass(pygame.sprite.Sprite):
+class cameraClass():
+	def __init__(self, scalingReference=[1/2, 10]):
+
+		self.pos = [0, 0, 0]
+		
+		self.scalingFactor = 1/scalingReference[0]**(0-scalingReference[1])
+	
+
+	def getScaling(self, distance=0):
+		# gets called by other objects when they're determining their size and position when drawing themselves
+		return scalingFactor**(self.pos[2]-distance)
