30a8860d12da2ae3cd0890f88aa42ed183ab1f0b
Jane Doe <paul.d.kolz@gmail.com>
2026-08-31 11:06:43 -0500

    Update main-window.py

--- main-window.py
+++ main-window.py
@@ -2,9 +2,16 @@ import pygame
 import random
 
 pygame.init()
-screen = pygame.display.set_mode((1600, 900))
-clock = pygame.time.Clock()
+
+refScreenSize = [1600, 900]
+screen = pygame.display.set_mode(refScreenSize)
+resolutionScaling = screen.get_height()/refScreenSize[1]
+resolutionScaling_alt = screen.get_width()/refScreenSize[0]
+
 pygame.display.set_caption("PLATFORMER")
+
+clock = pygame.time.Clock()
+
 
 
 
@@ -14,9 +21,10 @@ class cameraClass():
 
 		self.pos = [0, 0, 0]
 		
-		self.scalingFactor = 1/scalingReference[0]**(0-scalingReference[1])
+		self.scalingFactor = scalingReference[0]**(1/scalingReference[1])
 	
 
 	def getScaling(self, distance=0):
 		# gets called by other objects when they're determining their size and position when drawing themselves
-		return scalingFactor**(self.pos[2]-distance)
+		camScaling = self.scalingFactor**(self.pos[2]-distance)
+		return camScaling
