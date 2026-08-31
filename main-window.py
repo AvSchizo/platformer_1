2a78b95d7dfd8a5109940ae22b0259fa8fde9dbb
Paul Kolz <paul.d.kolz@gmail.com>
2026-08-30 22:01:39 -0500

    level data

--- main-window.py
+++ main-window.py
@@ -5,3 +5,8 @@ pygame.init()
 screen = pygame.display.set_mode((1600, 900))
 clock = pygame.time.Clock()
 pygame.display.set_caption("PLATFORMER")
+
+
+
+
+class playerSpriteClass(pygame.sprite.Sprite):
